12e2a279488c2da193b503c5ee4088285638d14f
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-12-04 13:52:43 +0900

    [finish]model relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+	belongs_to :user
+	has_many :likes, dependent: :destroy
+    def liked_by?(user)
+      likes.where(user_id: user.id).exists?
+    end
+	belongs_to :interest
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	has_many :favorites, dependent: :destroy
+	has_many :posts, dependent: :destroy
+	has_many :likes, dependent: :destroy
+	def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
+	belongs_to :nationality
+	belongs_to :country
 end
