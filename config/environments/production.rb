8b29919eaede4be9b383251d2a07fe66d596f18d
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-12-10 19:37:53 +0900

    test heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Tricket_Japan_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_caching = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'masayatest777@gmail.com',
+    password:             'masaya777',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
