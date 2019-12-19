e384a181cabeec85860decab249a4a6556d2b6fe
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-12-19 17:12:04 +0900

    [finish]admin side index

--- app/controllers/admins/posts_controller.rb
+++ app/controllers/admins/posts_controller.rb
@@ -3,15 +3,21 @@ class Admins::PostsController < ApplicationController
 	before_action :authenticate_admin!
 
 	def index
-		@posts = Post.all.reverse
+		@posts = Post.with_deleted.reverse
 	end
 	def search
 		if params[:search_flag] == "1"
 			@posts = Post.where("created_at LIKE ?", "%#{params[:date_search]}%")
 		elsif params[:search_flag] == "2"
-			@posts = Post.joins(:user).where("name_user LIKE ?", "%#{params[:user_search]}%")
-		elsif params[:search_flag] == "3"
 			@posts = Post.where("title LIKE ?", "%#{params[:title_search]}%")
+		elsif params[:search_flag] == "3"
+			if params[:delete_flag] == "0"
+				@posts = Post.with_deleted.joins(:user).where("name_user LIKE ?", "%#{params[:user_search]}%")
+			elsif params[:delete_flag] == "1"
+				@posts = Post.joins(:user).where("name_user LIKE ?", "%#{params[:user_search]}%")
+			elsif params[:delete_flag] == "2"
+				@posts = Post.only_deleted.joins(:user).where("name_user LIKE ?", "%#{params[:user_search]}%")
+			end
 		end
 		render :index
 	end
--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -3,23 +3,61 @@ class Admins::UsersController < ApplicationController
 	before_action :authenticate_admin!
 
 	def index_traveler
-
+		@users = User.where(type_user: 0).reverse
 	end
 	def search_traveler
-
+		if params[:search_flag] == "1"
+			if params[:delete_flag] == "0"
+				@users = User.where(type_user: 0).with_deleted.where("name_first || name_last LIKE ?", "%#{params[:user_search]}%")
+			elsif params[:delete_flag] == "1"
+				@users = User.where(type_user: 0).where("name_first || name_last LIKE ?", "%#{params[:user_search]}%")
+			elsif params[:delete_flag] == "2"
+				@users = User.where(type_user: 0).only_deleted.where("name_first || name_last LIKE ?", "%#{params[:user_search]}%")
+			end
+		elsif params[:search_flag] == "2"
+			if params[:delete_flag] == "0"
+				@users = User.where(type_user: 0).with_deleted.where("name_user LIKE ?", "%#{params[:user_name_search]}%")
+			elsif params[:delete_flag] == "1"
+				@users = User.where(type_user: 0).where("name_user LIKE ?", "%#{params[:user_name_search]}%")
+			elsif params[:delete_flag] == "2"
+				@users = User.where(type_user: 0).only_deleted.where("name_user LIKE ?", "%#{params[:user_name_search]}%")
+			end
+		end
+		render :index_traveler
 	end
+
 	def sort_traveler
 
 	end
-	def index_local
 
+	def index_local
+		@users = User.where(type_user: 1).reverse
 	end
 	def search_local
-
+		if params[:search_flag] == "1"
+			if params[:delete_flag] == "0"
+				@users = User.where(type_user: 1).with_deleted.where("name_first || name_last LIKE ?", "%#{params[:user_search]}%")
+			elsif params[:delete_flag] == "1"
+				@users = User.where(type_user: 1).where("name_first || name_last LIKE ?", "%#{params[:user_search]}%")
+			elsif params[:delete_flag] == "2"
+				@users = User.where(type_user: 1).only_deleted.where("name_first || name_last LIKE ?", "%#{params[:user_search]}%")
+			end
+		elsif params[:search_flag] == "2"
+			if params[:delete_flag] == "0"
+				@users = User.where(type_user: 1).with_deleted.where("name_user LIKE ?", "%#{params[:user_name_search]}%")
+			elsif params[:delete_flag] == "1"
+				@users = User.where(type_user: 1).where("name_user LIKE ?", "%#{params[:user_name_search]}%")
+			elsif params[:delete_flag] == "2"
+				@users = User.where(type_user: 1).only_deleted.where("name_user LIKE ?", "%#{params[:user_name_search]}%")
+			end
+		end
+		render :index_local
 	end
+
 	def sort_local
 
 	end
+
 	def show
 
 	end
