1d621564ad610d72b4405eceb1a53109ab91ce9d
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-12-25 21:17:14 +0900

    [modify]models

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,8 @@ class Notification < ApplicationRecord
 	belongs_to :comment, optional: true
 	belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
 	belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+
+  def self.unchecked_notification(current_user)
+    self.where(checked: false).where(visited_id: current_user.id)
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,7 @@ class Room < ApplicationRecord
   def self.new_message(current_user)
       self.order("updated_at desc").joins(:room_users).where("room_users.user_id = ?", current_user.id).distinct
   end
+  def self.unopened_message(current_user)
+      self.joins(:room_users).where("room_users.user_id = ?", current_user.id).map(&:messages).flatten(1).select {|message| message.status_open == 0 && message.user_id != current_user.id }
+  end
 end
