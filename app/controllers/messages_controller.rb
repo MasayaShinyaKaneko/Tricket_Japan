f6061203b433bf22aa70f3fbd32aedeb2f6be79b
MasayaShinyaKaneko <mkanekodays@gmail.com>
2020-01-04 22:43:09 +0900

    [ontheway]Rspec

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,21 @@
 class MessagesController < ApplicationController
 
-	before_action :authenticate_user!
+# 下記はactioncableを使用しているため、room_channelに記載してある。
 
-	def create
-	  if RoomUser.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-			Room.find(params[:message][:room_id]).touch
-			@message = Message.create(message_params.merge(:user_id => current_user.id))
-			redirect_to room_path(@message.room_id)
-	  else
-	   	redirect_to user_path(current_user)
-	  end
-	end
+	# before_action :authenticate_user!
 
-	private
-		def message_params
-			params.require(:message).permit(:user_id, :room_id, :content)
-		end
+	# def create
+	#   if RoomUser.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+	# 		Room.find(params[:message][:room_id]).touch
+	# 		@message = Message.create(message_params.merge(:user_id => current_user.id))
+	# 		redirect_to room_path(@message.room_id)
+	#   else
+	#    	redirect_to user_path(current_user)
+	#   end
+	# end
+
+	# private
+	# 	def message_params
+	# 		params.require(:message).permit(:user_id, :room_id, :content)
+	# 	end
 end
--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe NotificationsController, type: :controller do
 
+  describe "GET #index" do
+    context "as a logged in user" do
+      before do
+        @user = create(:user)
+        sign_in @user
+      end
+      it "responds successfully" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+    context "as a guest" do
+      it "returns a 302 response" do
+        get :index
+        expect(response).to have_http_status "302"
+      end
+      it "redirects to the sign-in page" do
+        get :index
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
